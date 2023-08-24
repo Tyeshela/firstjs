3351ab7d4eb9249bf58d90a808e24078a5614628
Tyeshela <120570882+Tyeshela@users.noreply.github.com>
2023-08-24 03:31:00 -0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,8 @@
+var i = document.getElementsByTagName("h1");
+for (var j = 0; j < i.length; j++) {
+  i[j].style.color = "#ff0000";
+}
+var x = document.getElementsByTagName("h2");
+for (var y = 0; y < x.length; y++) {
+  x[y].style.color = "#00ff00";
+}
